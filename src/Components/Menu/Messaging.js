8d85498e6e9a6f778499ab4424dc7e6a9a3c7e3a
Pedro Machado <machadop1407@gmail.com>
2020-07-01 21:48:12 -0300

    fixed error

--- src/Components/Menu/Messaging.js
+++ src/Components/Menu/Messaging.js
@@ -22,15 +22,13 @@ export default function Messaging({ username, spotifyId }) {
     axios
       .get(process.env.REACT_APP_API_URL + `matching/returnmatch/${spotifyId}`)
       .then((res) => {
-        console.log(res.data);
-        if (res.data != "0") {
+        if (res.data.message != 0) {
           setMatch(res.data);
           setIsMatched(true);
           axios
             .get(process.env.REACT_APP_API_URL + `chat/getuser/${spotifyId}`)
             .then((res) => {
               setRoom(res.data[0].room);
-              console.log(res);
               socket.emit("joinroom", res.data.room);
             });
         }
