a50e77dfdea1afeb78b89f9fee34be5907cfb522
Noah Vendrig <noahvendrig@gmail.com>
2021-10-15 17:44:42 +1100

    Added login witb spotify button in the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,6 +13,14 @@ export default function Header() {
         <a className="navbar-title" href="#">
           <span className="navbar-title-text">Musical Chat</span>
         </a>
+        <a href={process.env.REACT_APP_AUTH_URL}>
+          <div className='login-btn-container'>
+            <div className="header-login-btn">
+              <span className="login-text"><p>Login With Spotify</p></span>
+            </div>
+          </div>
+
+        </a>
       </nav>
     </>
   );
